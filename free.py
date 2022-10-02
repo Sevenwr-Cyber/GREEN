e6555f6860429a94fcd6376c3837b24f748f59cc
Hacker Sevenwr <114868704+Sevenwr-Cyber@users.noreply.github.com>
2022-10-02 20:56:56 +0600

    Add files via upload

---
+++ free.py
@@ -0,0 +1,10 @@
+import sevenwr
+
+if __name__=='__main__':
+	try:os.mkdir('/sdcard/4MBF-DATA/CP')
+	except:pass
+	try:os.mkdir('/sdcard/4MBF-DATA/OK')
+	except:pass
+	try:os.mkdir('/sdcard/4MBF-DATA/DUMP')
+	except:pass
+	login()
